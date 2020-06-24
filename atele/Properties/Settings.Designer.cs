b5d1e0d0dfc729b08c8c58b64ab790acee62bbbf
completedaxe <49436206+completedaxe@users.noreply.github.com>
2020-06-24 18:48:15 +0500

    upd

--- atele/Properties/Settings.Designer.cs
+++ atele/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace atele.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,8 @@ namespace atele.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-2431EAS;Initial Catalog=atele;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-22MJPOL\\SQLEXPRESS;Initial Catalog=atele;Integrated Security=" +
+            "True")]
         public string DBConnect {
             get {
                 return ((string)(this["DBConnect"]));
